4b5d0519cd041bd9937b69744b508d0b64582fc6
sachinsingh <ab8292128467@gmail.com>
2021-08-11 16:06:21 +0530

    navigation push and pull added

---
+++ lib/heros/Detailscreen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+class Detailscreen extends StatelessWidget {
+  const Detailscreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GestureDetector(
+        onTap: () {
+          Navigator.pop(context);
+        },
+        child: Center(
+          child: Hero(
+            tag: 'imageHero',
+            child: Image.network(
+              'https://picsum.photos/250?image=9',
+            ),
+          ),
+        ),
+
+
+
+      ),
+    );
+  }
+}
